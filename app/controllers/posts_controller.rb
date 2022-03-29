6079069b0628ff356592b2db548e3211bc1ab2b5
Meri <mgogi13@freeuni.edu.ge>
2022-03-29 19:51:16 +0400

    Add CRUD to post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,33 @@
 class PostsController < ApplicationController
-  def show; end
 
-  def index; end
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def index;
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(params.require(:post).permit(:title, :text))
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update(params.require(:post).permit(:title, :text))
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+  end
+
 end
