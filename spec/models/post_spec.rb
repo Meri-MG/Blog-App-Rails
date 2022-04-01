8d299870bcea0c3f2500e960451bfbb6f2e69486
Meri <mgogi13@freeuni.edu.ge>
2022-04-01 19:05:06 +0400

    Add tests for user and post models

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { Post.new(title: 'A post', text: 'Post text') }
+  # , comments_counter: 3, likes_counter: 1
+
+  before { subject.save }
+
+  describe "Associations" do
+    it { should belong_to(:author).without_validating_presence }
+  end
+
+  it 'isn\'t valid with characters less than 3' do
+    subject.title = 'Be'
+    expect(subject).to_not be_valid
+  end
+
+  it 'title should be present' do
+    subject.title = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'the likes_counter to be an integer' do
+    subject.likes_counter = 3
+    expect(subject.likes_counter).to be_integer
+  end
+
+  it 'the comments_counter to be an integer' do
+    subject.comments_counter = 0
+    expect(subject.comments_counter).to be_integer
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { User.new(name: 'Nino', photo: 'photo', bio: 'She lives in Georgia', posts_counter: 3) }
+
+  before { subject.save }
+
+  it 'isn\'t valid without a name' do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'the posts_counter to be 3' do
+    expect(subject.posts_counter).to be(3)
+  end
+
+  it 'the posts_counter to be an integer' do
+    expect(subject.posts_counter).to be_integer
+  end
+
+  # it 'the  to be an integer' do
+  #   subject.three_recent_posts = 0
+  #   expect(subject.posts_counter).to be_integer
+  # end
+
 end
