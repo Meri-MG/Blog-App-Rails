14f53a6b6d0ce49ef15fe8eb793fac855c2e98b9
Meri <mgogi13@freeuni.edu.ge>
2022-04-01 17:28:50 +0400

    Add validation for user's name and posts_counter

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   has_many :likes, foreign_key: 'author_id'
   has_many :comments, foreign_key: 'author_id'
   has_many :posts, foreign_key: 'author_id'
+  validates :name, presence: true
+  validates :posts_counter, comparison: { greater_than_or_equal_to: 0 }
 
   def three_recent_posts
     posts.order('created_at DESC').limit(3)
