a1b2390979fb5733d19f391e412939635ee3d854
Meri <mgogi13@freeuni.edu.ge>
2022-04-01 17:33:28 +0400

    Add validations for posts title, and counters

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   has_many :comments, foreign_key: 'author_id'
   has_many :posts, foreign_key: 'author_id'
   validates :name, presence: true
-  validates :posts_counter, comparison: { greater_than_or_equal_to: 0 }
+  validates :posts_counter, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
 
   def three_recent_posts
     posts.order('created_at DESC').limit(3)
