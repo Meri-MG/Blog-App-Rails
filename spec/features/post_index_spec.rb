38ab2c8907fa3dbb9d8535ca7acbaf5b98b82892
Bohdan Shcherbak <b.szczerbak@gmail.com>
2022-04-07 22:19:26 +0200

    Fix linter errors for post index spec and post show spec

--- spec/features/post_index_spec.rb
+++ spec/features/post_index_spec.rb
@@ -1,17 +1,19 @@
 require 'rails_helper'
 
-RSpec.feature "Users", type: :system do
-  describe 'Show Page' do 
+# rubocop:disable Metrics/BlockLength
+
+RSpec.feature 'Users', type: :system do
+  describe 'Show Page' do
     let!(:user) do
       visit new_user_session_path
       user = User.create(
-      name: 'Meri',
-      id: 1,
-      email: 'meri2@example.com',
-      password: 'password',
-      posts_counter: 4,
-      confirmed_at: Time.now
-    )
+        name: 'Meri',
+        id: 1,
+        email: 'meri2@example.com',
+        password: 'password',
+        posts_counter: 4,
+        confirmed_at: Time.now
+      )
       Post.create(
         author: user,
         title: 'my post',
@@ -19,52 +21,52 @@ RSpec.feature "Users", type: :system do
         likes_counter: 8,
         comments_counter: 6
       )
-    6.times { |comment| Comment.create(author: user, text: 'my comment') }
+      6.times { |_comment| Comment.create(author: user, text: 'my comment') }
 
-    8.times { |like| Like.create(author: user) }
+      8.times { |_like| Like.create(author: user) }
 
-    fill_in 'user_email', with: 'meri2@example.com'
-    fill_in 'user_password', with: 'password'
-    click_button 'Log in'
-    visit '/users/#{user.id}'
-    click_link user.posts_counter
-    visit user_path(user)
-    click_link 'Show all posts'
-    user
-  end
+      fill_in 'user_email', with: 'meri2@example.com'
+      fill_in 'user_password', with: 'password'
+      click_button 'Log in'
+      visit "/users/#{user.id}"
+      click_link user.posts_counter
+      visit user_path(user)
+      click_link 'Show all posts'
+      user
+    end
 
-    it "should check the content of user attributes on the page" do
+    it 'should check the content of user attributes on the page' do
       expect(page).to have_content(user.name)
       expect(page).to have_content(user.bio)
       expect(Post.count).to eq 1
       page.should have_selector('img')
     end
 
-    it "should check the content of post and comment" do
+    it 'should check the content of post and comment' do
       page.should have_content('my post text')
       posts = user.posts
-        posts.each do |post|
-          post.five_recent_comments.each do |comment|
-            expect(page).to have_content(comment.text)
-          end
+      posts.each do |post|
+        post.five_recent_comments.each do |comment|
+          expect(page).to have_content(comment.text)
         end
+      end
     end
 
-    it "should check the likes counter" do
+    it 'should check the likes counter' do
       posts = user.posts
       posts.each do |post|
         expect(page).to have_content(post.likes_counter)
       end
     end
 
-    it "should check the comments counter" do
+    it 'should check the comments counter' do
       posts = user.posts
       posts.each do |post|
         expect(page).to have_content(post.comments_counter)
       end
     end
 
-    it "should click on post and direct to the post show page" do
+    it 'should click on post and direct to the post show page' do
       click_link 'my post text'
       posts = user.posts
       posts.each do |post|
@@ -73,3 +75,5 @@ RSpec.feature "Users", type: :system do
     end
   end
 end
+
+# rubocop:enable Metrics/BlockLength
--- spec/features/post_show_spec.rb
+++ spec/features/post_show_spec.rb
@@ -1,17 +1,19 @@
 require 'rails_helper'
 
-RSpec.feature "Users", type: :system do
-  describe 'Show Page' do 
+# rubocop:disable Metrics/BlockLength
+
+RSpec.feature 'Users', type: :system do
+  describe 'Show Page' do
     let!(:post) do
       visit new_user_session_path
       user = User.create(
-      name: 'Meri',
-      id: 1,
-      email: 'meri2@example.com',
-      password: 'password',
-      posts_counter: 4,
-      confirmed_at: Time.now
-    )
+        name: 'Meri',
+        id: 1,
+        email: 'meri2@example.com',
+        password: 'password',
+        posts_counter: 4,
+        confirmed_at: Time.now
+      )
       post = Post.create(
         author: user,
         title: 'my post',
@@ -20,46 +22,48 @@ RSpec.feature "Users", type: :system do
         comments_counter: 6
       )
 
-    @comment = Comment.create(text: 'my comment', author: user, post: post)
+      @comment = Comment.create(text: 'my comment', author: user, post: post)
 
-    8.times { |like| Like.create(author_id: user) }
+      8.times { |_like| Like.create(author_id: user) }
 
-    fill_in 'user_email', with: 'meri2@example.com'
-    fill_in 'user_password', with: 'password'
-    click_button 'Log in'
-    visit '/users/#{user.id}'
-    click_link user.posts_counter
-    visit user_path(user)
-    click_link 'Show all posts'
-    click_link 'my post text'
-    post
-  end
+      fill_in 'user_email', with: 'meri2@example.com'
+      fill_in 'user_password', with: 'password'
+      click_button 'Log in'
+      visit "/users/#{user.id}"
+      click_link user.posts_counter
+      visit user_path(user)
+      click_link 'Show all posts'
+      click_link 'my post text'
+      post
+    end
 
     it "should have post's title, author" do
-       expect(page).to have_content(post.title)
-       page.should have_content('Meri')
+      expect(page).to have_content(post.title)
+      page.should have_content('Meri')
     end
 
-    it "should check posts text" do
-      page.should have_content("my post text")
+    it 'should check posts text' do
+      page.should have_content('my post text')
     end
 
-    it "should check posts likes" do
-        page.should have_content(8)
+    it 'should check posts likes' do
+      page.should have_content(8)
     end
 
-    it "should check posts comments" do
-        page.should have_content(7)
+    it 'should check posts comments' do
+      page.should have_content(7)
     end
 
-    it "should check comments author" do
+    it 'should check comments author' do
       comment = post.comments.first
       expect(page).to have_content(comment.author.name)
     end
 
-    it "should check comments text" do
+    it 'should check comments text' do
       comment = post.comments.first
       expect(page).to have_content(comment.text)
     end
   end
 end
+
+# rubocop:enable Metrics/BlockLength
