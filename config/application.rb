17d8d9357891b328b480a0fac4a2744e31bc7a52
Meri <mgogi13@freeuni.edu.ge>
2022-08-18 11:31:01 +0400

    fix: rufo formatting

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,8 @@ module BlogApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    config.api_only = true
+    config.middleware.use ActionDispatch::Flash
 
     # Configuration for the application, engines, and railties goes here.
     #
